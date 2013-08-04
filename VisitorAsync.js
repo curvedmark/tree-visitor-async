07d74ec6ece5188092da2d4ea3c7b1d9fbd331bc
Glen Huang <curvedmark@gmail.com>
2013-08-04 13:41:31 +0800

    refactor

--- VisitorAsync.js
+++ VisitorAsync.js
@@ -1,5 +1,6 @@
 var Visitor = require('tree-visitor');
 var Promise = require('promise-now');
+var _visitNode = Visitor.prototype._visitNode;
 
 module.exports = VisitorAsync;
 
@@ -7,14 +8,13 @@ function VisitorAsync() {}
 VisitorAsync.prototype = new Visitor();
 
 VisitorAsync.prototype._visitNodes = function (nodes) {
-	var promise = this._visitNode();
+	var promise = new Promise().fulfill(undefined, this);
 	for (var i = 0, len = nodes.length; i < len; ++i) {
-		promise = promise.then(this._visitNode.bind(this, nodes[i]));
+		promise = promise.then(_visitNode.bind(this, nodes[i]));
 	}
 	return promise.then(function () { return nodes; });
 };
 
-var _visitNode = Visitor.prototype._visitNode;
 VisitorAsync.prototype._visitNode = function (node) {
 	var promise = new Promise().fulfill(undefined, this);
 	return promise.then(function () {
